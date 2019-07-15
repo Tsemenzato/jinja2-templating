07ba3c28ae14f1fe41bf54b10e31fe8ee4e7bd07
Tsemenzato <32571165+Tsemenzato@users.noreply.github.com>
2019-07-15 11:10:06 -0300

    added template (#2)

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ env = Environment(
 
 template = env.get_template('receipt.html')
 
-print(
-    template.render({
-        "name": "John",
-        "test": 1
-    })
-)
+output = template.render({
+ "category": "Meals",
+ "date": "June 23,2019",
+ "merchant": "Momofuko Ko",
+ "total": 560.75,
+ "purpose": "",
+ "attendees": "<missing data>",
+ "attendees_list": ["atendee 1","atendee 2", "atendee 3"]
+})
+
+with open('my_new_html_file.html', 'w') as f:
+    f.write(output)
